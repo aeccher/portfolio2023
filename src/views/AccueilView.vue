b931edb7c9ff9eb8312e1c170ca698d404d1f36b
aeccher <95070759+aeccher@users.noreply.github.com>
2023-03-03 09:51:11 +0100

    ajout HR accueil

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,17 +1,20 @@
 <script>
 import AboutView from './AboutView.vue';
 import WorkView from './WorkView.vue';
+import ContactView from './ContactView.vue';
 
 
 
 export default {
-  components: { AboutView, WorkView},
+  components: { AboutView, WorkView, ContactView},
 }
 </script>
 
 <template>
   <main>
+    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
     <about-view/>
+    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
     <WorkView/>
 </main>
 </template>
