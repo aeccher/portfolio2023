91dfc04f770bed59c45674f6377fb765a9e3677e
aeccher <95070759+aeccher@users.noreply.github.com>
2023-03-08 10:28:35 +0100

    Modifications de la page des Mentions Légales

--- src/views/MentionsLegalesView.vue
+++ src/views/MentionsLegalesView.vue
@@ -3,25 +3,32 @@
     <h2 class="font-Lato text-3xl text-center font-bold mt-8 text-gray-600 underline underline-offset-8 decoration-double p-2">Mentions Légales</h2>
     <p class="font-Roboto text-xl p-4">Cette page décrit les mentions légales qui s'appliquent à tout internaute visitant ce site. En le consultant vous vous engagez sans réserve à les respecter. Les mentions légales pouvant être modifiées à tout moment et sans préavis, nous vous engageons à les consulter régulièrement.</p>
 
-    <h3 class="font-Roboto text-xl font-bold mt-8 text-gray-600 p-4"> Responsable éditorial et responsable légal</h3>
+    <h3 class="font-Roboto text-2xl font-bold mt-2 text-gray-600 p-4">Éditeur du site</h3>
     <ul class="list-disc font-Roboto text-xl ml-10">
         <li>Propriétaire : Agathe ECCHER</li>
-        <li>Hébergeur : Infomaniak</li>
+        <li>E-mail : <a href="mailto:agathe.eccher@edu.univ-fcomte.fr"><span class="text-green-500 underline">agathe.eccher@edu.univ-fcomte.fr</span></a></li>
     </ul>
 
-    <h3 class="font-Roboto text-xl font-bold mt-8 text-gray-600 p-4">Conditions d’utilisation</h3>
+    <h3 class="font-Roboto text-2xl font-bold mt-8 text-gray-600 p-4">Hébergeur</h3>
+    <p class="font-bold font-Roboto pl-4 pt-1 text-xl">Infomaniak</p>
+    <ul class="list-disc font-Roboto text-xl ml-10">
+        <li class="pt-2">Adresse : Les Acacias, Genève, Suisse - rue Eugène Marziano 251 227 Les Acacias (GE)</li>
+        <li class="pt-2">N° TVA : CHE-103.167.648 </li>
+    </ul>
+
+    <h3 class="font-Roboto text-2xl font-bold mt-8 text-gray-600 p-4">Conditions d’utilisation</h3>
     <p class="font-Roboto text-xl p-4 mr-4">
     Conformément à la loi Informatique et Libertés du 6 janvier 1978, vous disposez d'un droit d'accès et de rectification aux données vous concernant par simple demande mail, écrite ou téléphonique.
     Agathe ECCHER ne peut être tenue responsable du contenu du site. Ce site est exploité par Infomaniak. </p>
-    <p class="font-Roboto text-xl p-4 mr-4">La présentation et chacun des éléments, y compris les marques, logos et noms de domaine, apparaissant sur le site https://portfolio.agathe-eccher.fr sont protégés par les lois en vigueur sur la propriété intellectuelle, et appartiennent à Agathe ECCHER, ou font l'objet d'une autorisation d'utilisation. 
+    <p class="font-Roboto text-xl p-4 mr-4">La présentation et chacun des éléments, y compris les marques, logos et noms de domaine, apparaissant sur le site <span class="underline">https://portfolio.agathe-eccher.fr</span> sont protégés par les lois en vigueur sur la propriété intellectuelle, et appartiennent à Agathe ECCHER, ou font l'objet d'une autorisation d'utilisation. 
     Aucun élément composant le site ne peut être copié, reproduit, modifié, réédité, chargé, dénaturé, transmis ou distribué de quelque manière que ce soit, sous quelque support que ce soit, de façon partielle ou intégrale, sans l'autorisation écrite et préalable de Agathe ECCHER à l'exception d'une stricte utilisation pour les besoins de la presse et sous réserve du respect des droits de propriété intellectuelle et de tout autre droit de propriété dont il est fait mention. 
     Seule la copie à usage privé est autorisée pour votre usage personnel, privé et non-commercial, sur votre ordinateur personnel. Toute utilisation autorisée des éléments composant ou figurant dans le site doit se faire sans dénaturation, modification ou altération de quelque manière que ce soit. Agathe ECCHER se réserve le droit de poursuivre tout acte de contrefaçon de ses droits de propriété intellectuelle. Le présent Contrat est soumis au droit français. 
     Le présent Contrat s'applique dans le monde entier, et seule sa version en langue française fait foi en vue de son interprétation.</p>
 
     <ul class="list-disc font-Roboto text-xl p-4">
-        <li class="m-10"><span class="underline font-bold text-gray-600">Graphisme, photos et illustrations : </span>Le graphisme (incluant le webdesign et le logo) a été réalisé par Agathe ECCHER. Le portrait appartient à Imagia. Les autres éléments (images et vidéos) appartientent à Agathe ECCHER.</li>
-        <li class="m-10"><span class="underline font-bold text-gray-600">Déclaration à la CNIL : </span>Conformément à la loi 78-17 du 6 janvier 1978 (modifiée par la loi 2004-801 du 6 août 2004 relative à la protection des personnes physiques à l’égard des traitements de données à caractère personnel) relative à l’informatique, aux fichiers et aux libertés, ce site n’a pas fait l’objet d’une déclaration auprès de la Commission nationale de l’informatique et des libertés (www.cnil.fr).</li>
-        <li class="m-10"><span class="underline font-bold text-gray-600">Litiges :</span> Les présentes conditions du site https://portfolio.agathe-eccher.fr sont régies par les lois françaises et toute contestation ou litiges qui pourraient naître de l’interprétation ou de l’exécution de celles-ci seront de la compétence exclusive des tribunaux dont dépend le siège social de la société. La langue de référence, pour le règlement de contentieux éventuels, est le français.</li>
+        <li class="m-5"><span class="underline font-bold text-gray-600">Graphisme, photos et illustrations : </span>Le graphisme (incluant le webdesign et le logo) a été réalisé par Agathe ECCHER. Le portrait appartient à Imagia. Les autres éléments (images et vidéos) appartientent à Agathe ECCHER.</li>
+        <li class="m-5"><span class="underline font-bold text-gray-600">Déclaration à la CNIL : </span>Conformément à la loi 78-17 du 6 janvier 1978 (modifiée par la loi 2004-801 du 6 août 2004 relative à la protection des personnes physiques à l’égard des traitements de données à caractère personnel) relative à l’informatique, aux fichiers et aux libertés, ce site n’a pas fait l’objet d’une déclaration auprès de la Commission nationale de l’informatique et des libertés (<a class="text-green-500 underline" href="www.cnil.fr">www.cnil.fr</a>).</li>
+        <li class="m-5"><span class="underline font-bold text-gray-600">Litiges :</span> Les présentes conditions du site <span class="underline">https://portfolio.agathe-eccher.fr </span>sont régies par les lois françaises et toute contestation ou litiges qui pourraient naître de l’interprétation ou de l’exécution de celles-ci seront de la compétence exclusive des tribunaux dont dépend le siège social de la société. La langue de référence, pour le règlement de contentieux éventuels, est le français.</li>
     </ul>
 
 </template>
