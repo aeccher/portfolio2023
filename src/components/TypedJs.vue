48ce5ba4451667c0b66e16496ebced447f74427a
aeccher <95070759+aeccher@users.noreply.github.com>
2023-03-10 15:22:57 +0100

    Modifications des marges du dessus /contact + erreur mail /mentions-legales

--- src/components/TypedJs.vue
+++ src/components/TypedJs.vue
@@ -18,9 +18,9 @@ export default {
 </script>
 
 <template >
-  <div class="py-52">
+  <div class="py-48">
   <div id="typed" class="md:ml-16 text-8xl pb-10 align-text-center text-center font-Roboto text-green-500"></div>
-  <IconChevronsDown class="m-auto"/>
+  <IconChevronsDown class="m-auto mt-10"/>
   </div>
 </template>
 
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="text-gray-600 body-font relative mt-12 mb-40">
+    <section class="text-gray-600 body-font relative mt-16 lg:mt-28">
   <div class="container px-5 py-20 mx-auto">
     <div class="flex flex-col text-center w-full mb-12">
       <h2 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 font-Lato">Contactez-moi</h2>
