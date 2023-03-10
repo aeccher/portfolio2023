7bb99c075df7e2b874d194aea8d5013152949e27
aeccher <95070759+aeccher@users.noreply.github.com>
2023-03-10 09:15:27 +0100

    Header fixé

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Header -->
-  <header class="flex bg-green-500 ">
+  <header class="flex bg-green-500 fixed w-full mt-0">
     <nav class="flex items-center">
       <router-link to="/" class="flex items-center">
         <icon-logo class="ml-4" />
